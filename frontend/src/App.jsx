16174f8e2956b5c0b2041b392789b9fce28bf722
nour <nour.jmal2002@gmail.com>
2025-05-07 00:19:25 +0100

    backend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
 import UserPage from './pages/UserPage';
 import Home from './pages/Home'; 
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/user" element={<UserPage />} />
+        <Route path="/signup" element={<SignUp />} />
+
       </Routes>
     </Router>
   );
